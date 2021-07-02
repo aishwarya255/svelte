5196de66c09466a0a82ed16090db73b45a3843c0
aishwarya255 <84364801+aishwarya255@users.noreply.github.com>
2021-07-02 17:17:47 +0530

    svelte

---
+++ src/App.svelte
@@ -0,0 +1,637 @@
+
+        
+  
+        
+        
+        
+        
+<style>
+*{
+margin:0;
+padding:0;
+outline:none;
+}
+.menu
+    {
+        
+     width: 100%;
+     height: 30vh;
+     background-color: chocolate;    
+             
+    }
+ .header
+{
+	font-size: 25px;
+	color: white;
+	padding-left: 25px;
+	padding-top: 20px;
+	float: left;
+	font-family: sans-serif;
+    padding-top: 10vh;
+}
+ ul
+{
+	float: right;
+	margin-right: 50px;	
+
+}
+ul li
+{
+	display: inline-block;
+	padding: 10px;
+	margin-top: 12px;
+	cursor: pointer;
+	font-family:sans-serif;
+    padding-top: 5vh;
+     padding-bottom: 5vh;
+}
+ul li:hover
+{
+	background-color: white;
+	
+}
+ .cake img
+    {
+        float: left;
+        height: 30vh;
+        width: 30%;
+        padding-top: 2px;
+       
+        padding-left: 2px;
+        padding-right: 2px;
+    }
+ 
+.itemdet
+    {
+        float: left;
+        width: 80%;
+        height: 50vh;
+        margin-left: 10%;
+        font-size: 15px;
+        font-family: sans-serif;
+        background-color: chocolate;
+        padding-top: 1px;
+        margin-top: 1%;
+        
+    }
+    .itemdet img
+    {
+        float: left;
+        height: 40vh;
+        width: 30%;
+        padding-top: 3%;
+        padding-left: 3%;
+        padding-right: 3%;
+    }
+    .details
+    {
+        float: left;
+        width: 64%;
+        height: 50vh;
+        
+        
+    }
+    .namehead
+    {
+        font-family: sans-serif;
+        font-size: 22px;
+        padding-top: 4%;
+    }
+    .details p
+    {
+        padding-top: 20px;
+        font-family: sans-serif;
+        font-size: 18px;
+        line-height: 25px;
+        padding-left: 10px;
+        padding-right: 50px;
+        height: 30vh;
+        overflow: auto;
+        
+    }
+    
+    
+    
+    
+   .sign
+    {
+        width: 80%;
+        height: 10vh;
+        background-color: chocolate;
+        color: black;
+        font-family: sans-serif;
+        text-shadow:  5px 10px 30px rgba(0,0,0,0.7);
+        text-align: center;
+        padding-top: 5px;
+        margin-left: 120px;
+        
+        
+    }
+   #customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+    
+   margin-left: 30px;
+   margin-right: 30px;
+   margin-top: 1vh;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: chocolate;
+  color: white;
+} 
+      a {
+  text-decoration: none;
+  display: inline-block;
+  padding: 8px 16px;
+}
+
+a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.previous {
+  background-color: #f1f1f1;
+  color: black;
+}
+     
+    
+    
+    
+ .flex-container {
+  display: flex;
+  height: 100px;
+  align-items: center;
+  background-color: chocolate;
+}
+
+.flex-container > div {
+  background-color: #f1f1f1;
+  width: 100px;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
+}   
+ 
+    
+    
+     
+    .title
+{
+    float: left;
+	width:60%;
+	height:auto;
+	padding:10px;
+	margin-left:15%;
+	margin-top:4%;
+	border:none;
+	border-radius:5px;
+	box-shadow: 0 0px 10px 0 rgba(0,0,0,0.2);
+   
+
+}  
+    
+       
+    
+     .smt
+{
+	width:15%;
+	height:30px;
+    margin-top: 4%;
+	background-color:green;
+	color:white;
+	float:right;
+	margin-right:5%;
+   
+	border-radius:5px;
+	border:none;
+box-shadow: 0 0px 10px 0 rgba(0,0,0,0.2);
+}
+
+
+ 
+    
+
+    .about
+{
+  
+	float: left;
+      margin-top: 2%;
+	width: 34%;
+	height: 38vh;
+    background-color: chocolate;
+    color: white;
+    font-family: sans-serif;
+}
+
+.about p
+{
+  line-height: 25px;
+    font-size: 16px;
+	font-family: sans-serif;
+    color: white;
+    padding-left: 20px;
+    padding-right: 40px;
+    padding-top: 20px;
+}
+.contact
+{
+    float: left;
+    margin-top: 2%;
+	width: 33%;
+	height: 38vh;
+    background-color: chocolate;
+    color: white;
+    font-family: sans-serif;
+}
+.contactus
+    {
+        font-family: sans-serif;
+        font-size: 17px;
+        padding-left: 20px;
+        line-height: 25px;
+        font-weight: 200;
+        padding-top: 20px;
+    }
+
+.new
+{
+    float: left;
+      margin-top: 2%;
+	width: 33%;
+	height: 38vh;
+    background-color: chocolate;
+    color: white;
+    font-family: sans-serif;
+}
+.newup
+{
+	padding-left: 15px;
+	padding-right: 15%;
+	padding-top: 20px;
+	font-size: 16px;
+	font-family: sans-serif;
+    color: white;
+}
+    
+    marquee
+    {
+        
+        width: 100%;
+        height: 200px;
+        
+        
+    } 
+     
+   
+ </style>
+
+     <div class="menu">
+        
+        <h1 class="header">WECLOME TO OUR</h1>
+        <div class="cake img">
+         <img src="/img/cakelogo1.jpg"/>
+         
+         </div>
+        <ul>
+	<li onclick="window.location.href='#'"><h5>HOUSE</h5></li>
+	<li onclick="window.location.href='#btm'"><h5>ABOUT US</h5></li>
+	<li onclick="window.location.href='#btm'"><h5>CONTACTS</h5></li>
+    <li onclick="window.location.href='#btm'"><h5>NEW UPDATIONS</h5></li>
+    <li onclick="window.location.href='#btm'"><h5>LOGIN/SIGNUP</h5></li>
+     <li onclick="window.location.href='#'"><h5>PRICE</h5></li>
+</ul> 
+          <br>   <h3>(Yellow Butter Cake, Pound Cake, Red Velvet Cake, Carrot Cake, Sponge Cake, Genoise Cake, Chiffon Cake, Angel Food Cake, Flourless cake, Upside Down Cake, Devil's Food Cake, Hummingbird Cake, Opera Cake, Lady Baltimore Cake, Fruit Cake)</h3>  
+        </div> 
+    
+ <div class="itemdet">
+           <img src="/img/type1.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Yellow Butter Cake : </h5>
+                 
+            <p> Many a kid's birthday party is celebrated with a sheet-cake version of this dessert that's topped with billows of chocolate frosting and rainbow sprinkles. Yellow butter cakes often involve creaming the butter and sugar together (in modern times, this is done with an electric mixer) then adding the dry and wet ingredients, but there are also “dump” versions where all of the ingredients are thrown into a bowl and simply mixed together.</p>
+                
+ 
+             </div>
+         </div>
+   <div class="itemdet">
+           <img src="/img/type2.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Pound Cake : </h5>
+                 
+            <p> Named because it's made with a pound of each of the main ingredients (flour, butter, and sugar), this cake doesn’t rise much while baking, and the texture is very dense. Our version boasts matcha and cocoa powder, but classic recipes have nothing but vanilla extract to flavor it. Bake up one of these beauties in a loaf or Bundt pan.</p>
+                
+ 
+             </div>
+         </div>
+    
+ <div class="itemdet">
+           <img src="/img/type3.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Red Velvet Cake : </h5>
+                 
+            <p> This is a true favorite, especially in the south. Made with either butter or oil, red velvet cake's color classically comes from the reaction of buttermilk and cocoa powder. Modern versions often achieve this hue by using red food coloring, or in the case of the raspberry velvet cake, shown here, pink.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type4.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Carrot Cake : </h5>
+                 
+            <p> Leavened with baking soda and baking powder, this shortened cake uses oil as its main fat rather than butter. The addition of grated carrots makes the cake extra moist. Carrot cake is also flavored with warm spices and frosted with a rich cream cheese frosting. Pecans or walnuts are optional!</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type5.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Sponge Cake : </h5>
+                 
+            <p> This is a foam-style cake that has no artificial leaveners (baking powder or baking soda). It gets its volume solely from whipped eggs —either whole eggs or simply whites. This cake takes well to being soaked in a flavored syrup (try soaking with a lemon syrup and serve with a dollop of lemon curd), layered with whipped cream and smashed berries, or if baked up thin, rolled in the Christmas classic, Buche de Noel.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type6.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Genoise Cake : </h5>
+                 
+            <p> Genoise is what a sponge cake is called in Italy or France. In this cake, egg yolks, whites and sugar are beaten together until mousse-like. Next, flour, and either oil or butter, are folded in. This type of sponge cake is more moist and tender than its sponge cake cousin.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type7.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Chiffon Cake : </h5>
+                 
+            <p> Chiffon cake is a cross between a sponge and oil cake. The addition of oil gives it a richness similar to a shortened cake, and whipped egg whites and baking powder make it light and airy.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type8.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Angel Food Cake : </h5>
+                 
+            <p> The lightest of the light, angel food cake is made with only whipped egg whites for leavening and has no added fat. Baked in a special angel food cake pan, it’s often cooled upside down (in the pan) to help maintain the airy texture. The loads of sugar in an angel food cake give it a chewy and sponge-like texture.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type9.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Flourless Cake : </h5>
+                 
+            <p> This category includes both baked (think cheese cake or flourless chocolate) or unbaked (like mousse or unbaked cheesecakes) varieties. Typically very rich because of the high fat content, these cakes may or may not have a bottom crumb crust.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type10.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Upside Down Cake : </h5>
+                 
+            <p> The classic version of this butter style cake is traditionally made with pineapple, but can be made with many varieties of fruits, including plums, peaches, blueberries, or pears. The toppings are spread on the bottom of the pan, often with butter and sugar, and then topped with cake batter. After cooking, the cake is turned out so that the fruit is facing up.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type11.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Devil’s Food Cake : </h5>
+                 
+            <p> This cake is made "devilish" by the addition of chocolate in the form of cocoa powder. Extra baking soda causes the crumb to have more air bubbles, giving it a light and airy texture. This rich cake is frosted with either chocolate frosting or buttercream.</p>
+                
+ 
+             </div>
+         </div>
+    <div class="itemdet">
+           <img src="/img/type12.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Hummingbird Cake : </h5>
+                 
+            <p> The classic southern oil cake is flavored with banana, pineapple, pecans, lots of warm spices, and topped with a tangy cream cheese frosting. Created in Jamaica and originally called "Doctor Bird Cake,: this confection made its way Stateside sometime in the 1960s.</p>
+                
+ 
+             </div>
+         </div>
+       
+    
+     <div class="itemdet">
+           <img src="/img/type13.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Opera Cake : </h5>
+                 
+            <p> The history of this type of cake is a bit muddled. One thing we do know, however, is that its thin layers of coffee-soaked almond cake are sandwiched between an espresso-flavored buttercream, a bittersweet chocolate ganache, and topped off with a second rich chocolate ganache. Light but rich, this pastry is a real showstopper.</p>
+                
+ 
+             </div>
+         </div>
+        <div class="itemdet">
+           <img src="/img/type14.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Lady Baltimore Cake : </h5>
+                 
+            <p> Fluffy white cake is layered with a fruit and nut filling and topped with a pillowy meringue type frosting. The origin of the cake is somewhat in dispute, but it’s believed to have been created in the southern part of the United States.</p>
+                
+ 
+             </div>
+         </div>
+        <div class="itemdet">
+           <img src="/img/type15.jpg">
+             <div class="details">
+                 <h5 class="namehead"> Fruit Cake : </h5>
+                 
+            <p>Universally loathed and joked about, this holiday staple is full of dried or candied fruit, nuts, and spices. Once cooked, the cake is wrapped and soaked in a spirit like brandy or rum. (Despite the jokes, we think it's very delicious.)</p>
+                
+ 
+             </div>
+         </div>
+       
+       
+       <br>
+       
+     
+       <br>
+    
+    
+     
+ 
+   
+
+    
+    
+       
+       <table id="customers">  
+<tr>
+    <th>Cake_Name</th>
+    <th>Kg</th>
+    <th>Price</th>  
+   
+</tr>  
+
+<tr><td>Yellow Butter Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Pound Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Red Velvet Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Carrot Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Genoise Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Chiffon Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Angel Food Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+
+<tr><td>Flourless Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+
+    <tr><td>Upside Down Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Devils Food Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Hummingbird Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Opera Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Lady Baltimore Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+<tr><td>Fruit Cake</td>
+    <td>1</td>
+    <td>800</td>  
+   
+</tr>  
+</table> 
+       
+ <br/>      
+       
+   
+         
+         
+   <div class="bottum" id="btm">  
+ <div class="flex-container">
+  <div>S</div>
+  <div>I</div>
+  <div>G</div>  
+   <div>N</div>
+  <div>U</div>
+  <div>P</div>  
+  <div>_</div>
+  <div>H</div>
+  <div>E</div>
+  <div>R</div>
+  <div>E</div>
+</div>   
+    
+    
+        <form action="" method="post"> 
+            <input type="text" name="fullname" value=""  required placeholder="Fullname" class="title" />
+            <input type="text" name="address"  value=""  required placeholder="Address" class="title" />
+            <input type="text" name="phone" value=""  required placeholder="Phone number" class="title" />
+            <input type="text" name="uname" value=""  required placeholder="Username" class="title" />
+            <input type="password" name="password" value=""  required placeholder="Password" class="title" />
+             
+        <input type="submit" value="Submit" class="smt"/>  
+            </form>
+       <br><br>
+    </div>
+    
+    <div class="bottum" id="btm">
+    
+        <div class="about">
+
+           <h2>About us </h2>
+
+                <p>We are provided in good product.Its are also made up of our on borma.All are in puerly home made cakes.
+                    Correctly delived in correct time,</p>
+                <p>The coustomer are also do this given the details, Odered items Count the number of items  Address
+                    for the coustomer</p>
+
+            </div>
+
+        <div class="contact">
+
+            <h2>Contact us</h2>
+
+            <h5 class="contactus">Name:CAKE HOUSE<br>Address:Udumbannoor p.o,Thodupuzha<br>Contact num:9446991895,7558898939
+            <br>Email id:cakehouse123@gmail.com</h5>
+
+
+        </div>
+
+        <div class="new">
+
+            <h2>New Updations</h2>
+            <div class="newup"><marquee direction="up"  >New Updations......</marquee></div>
+
+        </div>
+    
+</div>     
+            
+        
